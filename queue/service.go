f7c4aeba281bf5ec2123f6cb949eaa0be2af9309
Nathan Blair <me@nathanblair.rocks>
2022-03-07 22:56:07 -0500

    more work to find runners for triaging jobs

--- queue/service.go
+++ queue/service.go
@@ -3,6 +3,9 @@ package queue
 import (
 	"conductor/generated"
 	"context"
+	"log"
+
+	"google.golang.org/grpc"
 )
 
 // Name is the name of the service
@@ -22,8 +25,48 @@ func (service *Service) Push(
 	ctx context.Context,
 	pipeline *generated.Pipeline,
 ) (_ *generated.Nil, err error) {
+	var availableRunners generated.ConfiguredRunners
+
+	var runnerOptions []grpc.DialOption
+
+	for _, eachRunner := range service.runners.Runners {
+		connection, err := grpc.Dial(eachRunner.Info.Address, runnerOptions)
+		if err != nil {
+			log.Printf("%v", err)
+			continue
+		}
+
+		defer connection.Close()
+		runnerClient := generated.NewRunnerClient(connection)
+		runnerStatus, err := runnerClient.Status(ctx, &generated.Nil{})
+		if err != nil {
+			log.Printf("%v", err)
+			continue
+		}
+
+		runnerInfo, err := runnerClient.Info(ctx, &generated.Nil{})
+		if err != nil {
+			log.Printf("%v", err)
+			continue
+		}
+
+		// TODO Verify Runner Attributes
+
+		// TODO Verify Runner Status
+		// if runnerInfo.Attributes.Arch ==
+
+		// FIXME If the runner attributes fit the job
+		// AND the runner is listed as available...
+		// Then add the runner to the list of available runners
+		if true {
+			availableRunners.Runners = append(availableRunners.Runners, eachRunner)
+		}
+
+	}
+
 	// TODO From the list of configured runners for the queue,
-	// triage applicable jobs across them
+	// determine which runners are applicable and available for jobs,
+	// then triage applicable jobs across them
 
 	return
 }
